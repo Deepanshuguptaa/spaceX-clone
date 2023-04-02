7aecbbd1928d05e584062f3338ab4021181d8b95
Deepanshuguptaa <108191958+Deepanshuguptaa@users.noreply.github.com>
2023-04-02 18:24:32 +0530

    removed counter

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
-const counters = document.querySelectorAll('.counter');
+
 let scrollStarted = false;
 
 btn.addEventListener('click', navToggle);
@@ -26,34 +26,4 @@ function scrollPage() {
   }
 }
 
-function countUp() {
-  counters.forEach((counter) => {
-    counter.innerText = '0';
 
-    const updateCounter = () => {
-      // Get count target
-      const target = +counter.getAttribute('data-target');
-      // Get current counter value
-      const c = +counter.innerText;
-
-      // Create an increment
-      const increment = target / 100;
-
-      // If counter is less than target, add increment
-      if (c < target) {
-        // Round up and set counter value
-        counter.innerText = `${Math.ceil(c + increment)}`;
-
-        setTimeout(updateCounter, 75);
-      } else {
-        counter.innerText = target;
-      }
-    };
-
-    updateCounter();
-  });
-}
-
-function reset() {
-  counters.forEach((counter) => (counter.innerHTML = '0'));
-}
